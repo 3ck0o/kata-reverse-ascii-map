0bb79afa75a5feb1324f76f2d529544370e2271e
3ck0 <gaetan.lemoing@outlook.fr>
2017-08-05 17:54:19 +0200

    Init

---
+++ src/Main.kt
@@ -0,0 +1,7 @@
+fun main(arg: Array<String>) {
+    val chars = Array(96, { i -> (0x20 + i).toChar() })
+    while (!chars.all { c -> c == ' ' }) {
+        chars.forEach { c -> print(c); if (c.toInt() % 16 == 0) println() }
+        chars[readLine().toString()[0].toInt() - 0x20] = ' '
+    }
+}

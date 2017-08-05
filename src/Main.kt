6690a74828f1201bd15adb3359a1a0228832af4d
3ck0 <gaetan.lemoing@outlook.fr>
2017-08-05 18:05:26 +0200

    Optimized the code

--- src/Main.kt
+++ src/Main.kt
@@ -2,6 +2,6 @@ fun main(arg: Array<String>) {
     val chars = Array(96, { i -> (0x20 + i).toChar() })
     while (!chars.all { c -> c == ' ' }) {
         chars.forEach { c -> print(c); if (c.toInt() % 16 == 0) println() }
-        chars[readLine().toString()[0].toInt() - 0x20] = ' '
+        chars[readLine()!![0].toInt() - 0x20] = ' '
     }
 }
